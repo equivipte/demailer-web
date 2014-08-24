52edf19ca17daf32d17c4e0a39ae96a90650db6b
Teguh Wanrasagita <tsagita@hotmail.com>
2014-08-24 21:53:26 +0800

    comment out SubscriberManagementController mapping issue, caused app failed to start

--- src/main/java/com/equivi/mailsy/web/controller/SubscriberManagementController.java
+++ src/main/java/com/equivi/mailsy/web/controller/SubscriberManagementController.java
@@ -49,186 +49,186 @@ public class SubscriberManagementController extends AbstractController {
         sdf = new SimpleDateFormat(DATE_TIME_FORMAT);
     }
 
-    @RequestMapping(value = "/main/subscriber_management", method = RequestMethod.GET)
-    public String getContactManagementPage(Model model) {
-
-        List<SubscriberGroupDTO> subscriberGroupDTOList = new ArrayList<>();
-
-        SubscriberGroupDTO contactDTO = new SubscriberGroupDTO();
-        contactDTO.setSubscriberGroupName("Equivi Contact Group 1");
-        contactDTO.setSubscriberGroupStatus("ACTIVE");
-        contactDTO.setSubscriberLastUpdateDate("2014/07/30 10:00");
-
-        SubscriberGroupDTO contactDTO2 = new SubscriberGroupDTO();
-        contactDTO2.setSubscriberGroupName("Equivi Contact Group 1-1");
-        contactDTO2.setSubscriberGroupStatus("ACTIVE");
-        contactDTO2.setSubscriberLastUpdateDate("2014/07/30 13:00");
-
-        SubscriberGroupDTO contactDTO3 = new SubscriberGroupDTO();
-        contactDTO3.setSubscriberGroupName("Equivi Contact Group 2");
-        contactDTO3.setSubscriberGroupStatus("INACTIVE");
-        contactDTO3.setSubscriberLastUpdateDate("2014/07/30 11:00");
-
-        SubscriberGroupDTO contactDTO4 = new SubscriberGroupDTO();
-        contactDTO4.setSubscriberGroupName("Equivi Contact Group 3");
-        contactDTO4.setSubscriberGroupStatus("DISABLED");
-        contactDTO4.setSubscriberLastUpdateDate("2014/07/30 10:00");
-
-        subscriberGroupDTOList.add(contactDTO);
-        subscriberGroupDTOList.add(contactDTO2);
-        subscriberGroupDTOList.add(contactDTO3);
-        subscriberGroupDTOList.add(contactDTO4);
-
-        model.addAttribute("subscriberGroupDTOList", subscriberGroupDTOList);
-
-        return "subscriberManagementPage";
-    }
-
-
-    @RequestMapping(value = "/main/subscriber_management/{pageNumber}", method = RequestMethod.GET)
-    public String getSubscriberManagementPage(@PathVariable Integer pageNumber, final HttpServletRequest request, Model model) {
-
-        Page<SubscriberGroupEntity> subscriberGroupPage = subscriberService.listSubscriberGroup(buildMapFilter(request), pageNumber, webConfiguration.getMaxRecordsPerPage());
-
-        setSubscribeGroupList(model, subscriberGroupPage);
-
-        setPagination(model, subscriberGroupPage);
-
-        return "subscriberManagementPage";
-    }
-
-    @RequestMapping(value = "/main/subscriber_management/create", method = RequestMethod.GET)
-    public ModelAndView goToCreateSubscribeGroup(ModelAndView modelAndView) {
-
-        SubscriberGroupDTO subscriberGroupDTO = new SubscriberGroupDTO();
-
-        setPredefinedData(modelAndView, subscriberGroupDTO);
-
-        modelAndView.setViewName("subscriberManagementAddPage");
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/main/subscriber_management/subscriber_list/{subscriberGroupId}/{pageNumber}", method = RequestMethod.GET)
-    public ModelAndView goToEditSubscribeGroup(ModelAndView modelAndView, @PathVariable Long subscriberGroupId, @PathVariable Integer pageNumber) {
-
-        SubscriberGroupDTO subscriberGroupDTO = subscriberService.getSubscriberGroupAndSubscriberList(subscriberGroupId, pageNumber, webConfiguration.getMaxRecordsPerPage());
-        setPredefinedData(modelAndView, subscriberGroupDTO);
-
-        modelAndView.addObject("subscriberDTOList", subscriberGroupDTO.getSubscriberList());
-        modelAndView.addObject("subscriberGroupDTO", subscriberGroupDTO);
-        modelAndView.setViewName("subscriberManagementEditPage");
-        return modelAndView;
-    }
-
-
-    @RequestMapping(value = "/main/subscriber_management/saveAddSubscriberGroup", method = RequestMethod.POST)
-    public ModelAndView saveAddSubscriberGroup(@Valid SubscriberGroupDTO subscriberGroupDTO, HttpServletRequest httpServletRequest, BindingResult result, Locale locale) {
-
-        ModelAndView modelAndView;
-        try {
-            if (result.hasErrors()) {
-                modelAndView = new ModelAndView("subscriberManagementAddPage");
-                modelAndView.addObject("errors", errorMessage.buildFormValidationErrorMessages(result, locale));
-            } else {
-
-                setDefaultValue(subscriberGroupDTO);
-                SubscriberGroupEntity subscriberGroupEntity = subscriberService.saveSubscriberGroup(subscriberGroupDTO);
-
-                modelAndView = new ModelAndView();
-                String redirectData = "redirect:subscriber_list/" + subscriberGroupEntity.getId().toString() + "/1";
-
-                modelAndView.setViewName(redirectData);
-                return modelAndView;
-            }
-        } catch (InvalidDataException idex) {
-            modelAndView = new ModelAndView("subscriberManagementAddPage");
-
-            modelAndView.addObject("errors", errorMessage.buildServiceValidationErrorMessages(idex, locale));
-        }
-
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "/main/subscriber_management/saveUpdateSubscriberGroup", method = RequestMethod.POST)
-    public ModelAndView saveUpdateSubscriberGroup(@Valid SubscriberGroupDTO subscriberGroupDTO, HttpServletRequest httpServletRequest, BindingResult result, Locale locale) {
-
-        ModelAndView modelAndView;
-        try {
-            if (result.hasErrors()) {
-                modelAndView = new ModelAndView("subscriberManagementEditPage");
-                modelAndView.addObject("errors", errorMessage.buildFormValidationErrorMessages(result, locale));
-            } else {
-
-                String active = httpServletRequest.getParameter("subscribe-status");
-
-                if ("on".equals(active)) {
-                    subscriberGroupDTO.setSubscriberGroupStatus(GenericStatus.ACTIVE.getStatusDescription());
-                } else {
-                    subscriberGroupDTO.setSubscriberGroupStatus(GenericStatus.INACTIVE.getStatusDescription());
-                }
-                SubscriberGroupEntity subscriberGroupEntity = subscriberService.saveSubscriberGroup(subscriberGroupDTO);
-
-                modelAndView = new ModelAndView();
-                String redirectData = "redirect:/main/subscriber_management/1";
-
-                modelAndView.setViewName(redirectData);
-                return modelAndView;
-            }
-        } catch (InvalidDataException idex) {
-            modelAndView = new ModelAndView("subscriberManagementEditPage");
-
-            modelAndView.addObject("errors", errorMessage.buildServiceValidationErrorMessages(idex, locale));
-        }
-
-        return modelAndView;
-    }
-
-    private SubscriberGroupDTO setDefaultValue(SubscriberGroupDTO subscriberGroupDTO) {
-        subscriberGroupDTO.setSubscriberGroupStatus(GenericStatus.ACTIVE.getStatusDescription());
-        return subscriberGroupDTO;
-    }
-
-    private void setPredefinedData(ModelAndView modelAndView, SubscriberGroupDTO subscriberGroupDTO) {
-        modelAndView.addObject("subscriberGroupDTO", subscriberGroupDTO);
-    }
-
-    private void setSubscribeGroupList(Model model, Page<SubscriberGroupEntity> subscriberGroupEntityPage) {
-        model.addAttribute("subscriberGroupDTOList", convertToSubscribeGroupDTOList(subscriberGroupEntityPage.getContent()));
-    }
-
-    private List<SubscriberGroupDTO> convertToSubscribeGroupDTOList(List<SubscriberGroupEntity> subscriberGroupEntityList) {
-        List<SubscriberGroupDTO> subscriberGroupDTOList = new ArrayList<>();
-
-        if (subscriberGroupEntityList != null && !subscriberGroupEntityList.isEmpty()) {
-            for (SubscriberGroupEntity subscriberGroupEntity : subscriberGroupEntityList) {
-                SubscriberGroupDTO subscriberGroupDTO = new SubscriberGroupDTO();
-                subscriberGroupDTO.setId(subscriberGroupEntity.getId());
-                subscriberGroupDTO.setSubscriberGroupName(subscriberGroupEntity.getGroupName());
-                subscriberGroupDTO.setSubscriberGroupStatus(subscriberGroupEntity.getStatus().getStatusDescription());
-                subscriberGroupDTO.setSubscriberLastUpdateDate(sdf.format(subscriberGroupEntity.getLastUpdatedDateTime()));
-                subscriberGroupDTOList.add(subscriberGroupDTO);
-            }
-        }
-
-        return subscriberGroupDTOList;
-    }
-
-
-    private Map<SubscriberGroupSearchFilter, String> buildMapFilter(HttpServletRequest request) {
-
-        String groupName = request.getParameter(SubscriberGroupSearchFilter.SUBSCRIBER_GROUP_NAME.getFilterName());
-        String emailAddress = request.getParameter(SubscriberGroupSearchFilter.EMAIL_ADDRESS.getFilterName());
-
-
-        Map<SubscriberGroupSearchFilter, String> filterMap = new HashMap<>();
-        if (!StringUtils.isBlank(groupName)) {
-            filterMap.put(SubscriberGroupSearchFilter.SUBSCRIBER_GROUP_NAME, groupName);
-        }
-
-        if (!StringUtils.isBlank(emailAddress)) {
-            filterMap.put(SubscriberGroupSearchFilter.EMAIL_ADDRESS, emailAddress);
-        }
-
-        return filterMap;
-    }
+//    @RequestMapping(value = "/main/subscriber_management", method = RequestMethod.GET)
+//    public String getContactManagementPage(Model model) {
+//
+//        List<SubscriberGroupDTO> subscriberGroupDTOList = new ArrayList<>();
+//
+//        SubscriberGroupDTO contactDTO = new SubscriberGroupDTO();
+//        contactDTO.setSubscriberGroupName("Equivi Contact Group 1");
+//        contactDTO.setSubscriberGroupStatus("ACTIVE");
+//        contactDTO.setSubscriberLastUpdateDate("2014/07/30 10:00");
+//
+//        SubscriberGroupDTO contactDTO2 = new SubscriberGroupDTO();
+//        contactDTO2.setSubscriberGroupName("Equivi Contact Group 1-1");
+//        contactDTO2.setSubscriberGroupStatus("ACTIVE");
+//        contactDTO2.setSubscriberLastUpdateDate("2014/07/30 13:00");
+//
+//        SubscriberGroupDTO contactDTO3 = new SubscriberGroupDTO();
+//        contactDTO3.setSubscriberGroupName("Equivi Contact Group 2");
+//        contactDTO3.setSubscriberGroupStatus("INACTIVE");
+//        contactDTO3.setSubscriberLastUpdateDate("2014/07/30 11:00");
+//
+//        SubscriberGroupDTO contactDTO4 = new SubscriberGroupDTO();
+//        contactDTO4.setSubscriberGroupName("Equivi Contact Group 3");
+//        contactDTO4.setSubscriberGroupStatus("DISABLED");
+//        contactDTO4.setSubscriberLastUpdateDate("2014/07/30 10:00");
+//
+//        subscriberGroupDTOList.add(contactDTO);
+//        subscriberGroupDTOList.add(contactDTO2);
+//        subscriberGroupDTOList.add(contactDTO3);
+//        subscriberGroupDTOList.add(contactDTO4);
+//
+//        model.addAttribute("subscriberGroupDTOList", subscriberGroupDTOList);
+//
+//        return "subscriberManagementPage";
+//    }
+//
+//
+//    @RequestMapping(value = "/main/subscriber_management/{pageNumber}", method = RequestMethod.GET)
+//    public String getSubscriberManagementPage(@PathVariable Integer pageNumber, final HttpServletRequest request, Model model) {
+//
+//        Page<SubscriberGroupEntity> subscriberGroupPage = subscriberService.listSubscriberGroup(buildMapFilter(request), pageNumber, webConfiguration.getMaxRecordsPerPage());
+//
+//        setSubscribeGroupList(model, subscriberGroupPage);
+//
+//        setPagination(model, subscriberGroupPage);
+//
+//        return "subscriberManagementPage";
+//    }
+//
+//    @RequestMapping(value = "/main/subscriber_management/create", method = RequestMethod.GET)
+//    public ModelAndView goToCreateSubscribeGroup(ModelAndView modelAndView) {
+//
+//        SubscriberGroupDTO subscriberGroupDTO = new SubscriberGroupDTO();
+//
+//        setPredefinedData(modelAndView, subscriberGroupDTO);
+//
+//        modelAndView.setViewName("subscriberManagementAddPage");
+//        return modelAndView;
+//    }
+//
+//    @RequestMapping(value = "/main/subscriber_management/subscriber_list/{subscriberGroupId}/{pageNumber}", method = RequestMethod.GET)
+//    public ModelAndView goToEditSubscribeGroup(ModelAndView modelAndView, @PathVariable Long subscriberGroupId, @PathVariable Integer pageNumber) {
+//
+//        SubscriberGroupDTO subscriberGroupDTO = subscriberService.getSubscriberGroupAndSubscriberList(subscriberGroupId, pageNumber, webConfiguration.getMaxRecordsPerPage());
+//        setPredefinedData(modelAndView, subscriberGroupDTO);
+//
+//        modelAndView.addObject("subscriberDTOList", subscriberGroupDTO.getSubscriberList());
+//        modelAndView.addObject("subscriberGroupDTO", subscriberGroupDTO);
+//        modelAndView.setViewName("subscriberManagementEditPage");
+//        return modelAndView;
+//    }
+//
+//
+//    @RequestMapping(value = "/main/subscriber_management/saveAddSubscriberGroup", method = RequestMethod.POST)
+//    public ModelAndView saveAddSubscriberGroup(@Valid SubscriberGroupDTO subscriberGroupDTO, HttpServletRequest httpServletRequest, BindingResult result, Locale locale) {
+//
+//        ModelAndView modelAndView;
+//        try {
+//            if (result.hasErrors()) {
+//                modelAndView = new ModelAndView("subscriberManagementAddPage");
+//                modelAndView.addObject("errors", errorMessage.buildFormValidationErrorMessages(result, locale));
+//            } else {
+//
+//                setDefaultValue(subscriberGroupDTO);
+//                SubscriberGroupEntity subscriberGroupEntity = subscriberService.saveSubscriberGroup(subscriberGroupDTO);
+//
+//                modelAndView = new ModelAndView();
+//                String redirectData = "redirect:subscriber_list/" + subscriberGroupEntity.getId().toString() + "/1";
+//
+//                modelAndView.setViewName(redirectData);
+//                return modelAndView;
+//            }
+//        } catch (InvalidDataException idex) {
+//            modelAndView = new ModelAndView("subscriberManagementAddPage");
+//
+//            modelAndView.addObject("errors", errorMessage.buildServiceValidationErrorMessages(idex, locale));
+//        }
+//
+//        return modelAndView;
+//    }
+//
+//    @RequestMapping(value = "/main/subscriber_management/saveUpdateSubscriberGroup", method = RequestMethod.POST)
+//    public ModelAndView saveUpdateSubscriberGroup(@Valid SubscriberGroupDTO subscriberGroupDTO, HttpServletRequest httpServletRequest, BindingResult result, Locale locale) {
+//
+//        ModelAndView modelAndView;
+//        try {
+//            if (result.hasErrors()) {
+//                modelAndView = new ModelAndView("subscriberManagementEditPage");
+//                modelAndView.addObject("errors", errorMessage.buildFormValidationErrorMessages(result, locale));
+//            } else {
+//
+//                String active = httpServletRequest.getParameter("subscribe-status");
+//
+//                if ("on".equals(active)) {
+//                    subscriberGroupDTO.setSubscriberGroupStatus(GenericStatus.ACTIVE.getStatusDescription());
+//                } else {
+//                    subscriberGroupDTO.setSubscriberGroupStatus(GenericStatus.INACTIVE.getStatusDescription());
+//                }
+//                SubscriberGroupEntity subscriberGroupEntity = subscriberService.saveSubscriberGroup(subscriberGroupDTO);
+//
+//                modelAndView = new ModelAndView();
+//                String redirectData = "redirect:/main/subscriber_management/1";
+//
+//                modelAndView.setViewName(redirectData);
+//                return modelAndView;
+//            }
+//        } catch (InvalidDataException idex) {
+//            modelAndView = new ModelAndView("subscriberManagementEditPage");
+//
+//            modelAndView.addObject("errors", errorMessage.buildServiceValidationErrorMessages(idex, locale));
+//        }
+//
+//        return modelAndView;
+//    }
+//
+//    private SubscriberGroupDTO setDefaultValue(SubscriberGroupDTO subscriberGroupDTO) {
+//        subscriberGroupDTO.setSubscriberGroupStatus(GenericStatus.ACTIVE.getStatusDescription());
+//        return subscriberGroupDTO;
+//    }
+//
+//    private void setPredefinedData(ModelAndView modelAndView, SubscriberGroupDTO subscriberGroupDTO) {
+//        modelAndView.addObject("subscriberGroupDTO", subscriberGroupDTO);
+//    }
+//
+//    private void setSubscribeGroupList(Model model, Page<SubscriberGroupEntity> subscriberGroupEntityPage) {
+//        model.addAttribute("subscriberGroupDTOList", convertToSubscribeGroupDTOList(subscriberGroupEntityPage.getContent()));
+//    }
+//
+//    private List<SubscriberGroupDTO> convertToSubscribeGroupDTOList(List<SubscriberGroupEntity> subscriberGroupEntityList) {
+//        List<SubscriberGroupDTO> subscriberGroupDTOList = new ArrayList<>();
+//
+//        if (subscriberGroupEntityList != null && !subscriberGroupEntityList.isEmpty()) {
+//            for (SubscriberGroupEntity subscriberGroupEntity : subscriberGroupEntityList) {
+//                SubscriberGroupDTO subscriberGroupDTO = new SubscriberGroupDTO();
+//                subscriberGroupDTO.setId(subscriberGroupEntity.getId());
+//                subscriberGroupDTO.setSubscriberGroupName(subscriberGroupEntity.getGroupName());
+//                subscriberGroupDTO.setSubscriberGroupStatus(subscriberGroupEntity.getStatus().getStatusDescription());
+//                subscriberGroupDTO.setSubscriberLastUpdateDate(sdf.format(subscriberGroupEntity.getLastUpdatedDateTime()));
+//                subscriberGroupDTOList.add(subscriberGroupDTO);
+//            }
+//        }
+//
+//        return subscriberGroupDTOList;
+//    }
+//
+//
+//    private Map<SubscriberGroupSearchFilter, String> buildMapFilter(HttpServletRequest request) {
+//
+//        String groupName = request.getParameter(SubscriberGroupSearchFilter.SUBSCRIBER_GROUP_NAME.getFilterName());
+//        String emailAddress = request.getParameter(SubscriberGroupSearchFilter.EMAIL_ADDRESS.getFilterName());
+//
+//
+//        Map<SubscriberGroupSearchFilter, String> filterMap = new HashMap<>();
+//        if (!StringUtils.isBlank(groupName)) {
+//            filterMap.put(SubscriberGroupSearchFilter.SUBSCRIBER_GROUP_NAME, groupName);
+//        }
+//
+//        if (!StringUtils.isBlank(emailAddress)) {
+//            filterMap.put(SubscriberGroupSearchFilter.EMAIL_ADDRESS, emailAddress);
+//        }
+//
+//        return filterMap;
+//    }
 }
