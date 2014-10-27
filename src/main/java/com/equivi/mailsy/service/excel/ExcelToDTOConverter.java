3dfbc42b23a1198d21562d90fc84aadb68417570
Eldrid Aditya <zztop_aditya@yahoo.com>
2014-10-28 00:54:51 +0800

    Fix Subscriber import error and enable Subscriber Contact Entity list service without paging for temp

--- src/main/java/com/equivi/mailsy/service/excel/ExcelToDTOConverter.java
+++ src/main/java/com/equivi/mailsy/service/excel/ExcelToDTOConverter.java
@@ -14,40 +14,61 @@ public class ExcelToDTOConverter implements Serializable {
     private static final long serialVersionUID = 1494425012779885879L;
 
     public ContactDTO convertToContactDTO(Map<String, Integer> mapColumnIndex, ContactDTO contactDTO, Cell cell) {
-        if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.EMAIL_ADDRESS.getHeaderName())) {
-            contactDTO.setEmailAddress(cell.getStringCellValue());
+        if (mapColumnIndex.get(HeaderImportSubscribe.EMAIL_ADDRESS.getHeaderName()) != null) {
+            if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.EMAIL_ADDRESS.getHeaderName())) {
+                contactDTO.setEmailAddress(cell.getStringCellValue());
+            }
         }
-        if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.FIRST_NAME.getHeaderName())) {
-            contactDTO.setFirstName(cell.getStringCellValue());
+        if (mapColumnIndex.get(HeaderImportSubscribe.FIRST_NAME.getHeaderName()) != null) {
+            if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.FIRST_NAME.getHeaderName())) {
+                contactDTO.setFirstName(cell.getStringCellValue());
+            }
         }
-        if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.LAST_NAME.getHeaderName())) {
-            contactDTO.setLastName(cell.getStringCellValue());
+        if (mapColumnIndex.get(HeaderImportSubscribe.LAST_NAME.getHeaderName()) != null) {
+            if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.LAST_NAME.getHeaderName())) {
+                contactDTO.setLastName(cell.getStringCellValue());
+            }
         }
-        if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.COMPANY_NAME.getHeaderName())) {
-            contactDTO.setCompanyName(cell.getStringCellValue());
+        if (mapColumnIndex.get(HeaderImportSubscribe.COMPANY_NAME.getHeaderName()) != null) {
+            if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.COMPANY_NAME.getHeaderName())) {
+                contactDTO.setCompanyName(cell.getStringCellValue());
+            }
         }
-        if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.ADDRESS1.getHeaderName())) {
-            contactDTO.setAddress1(cell.getStringCellValue());
+        if (mapColumnIndex.get(HeaderImportSubscribe.ADDRESS1.getHeaderName()) != null) {
+            if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.ADDRESS1.getHeaderName())) {
+                contactDTO.setAddress1(cell.getStringCellValue());
+            }
         }
-        if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.ADDRESS2.getHeaderName())) {
-            contactDTO.setAddress2(cell.getStringCellValue());
+        if (mapColumnIndex.get(HeaderImportSubscribe.ADDRESS2.getHeaderName()) != null) {
+            if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.ADDRESS2.getHeaderName())) {
+                contactDTO.setAddress2(cell.getStringCellValue());
+            }
         }
-        if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.ADDRESS3.getHeaderName())) {
-            contactDTO.setAddress3(cell.getStringCellValue());
+        if (mapColumnIndex.get(HeaderImportSubscribe.ADDRESS3.getHeaderName()) != null) {
+            if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.ADDRESS3.getHeaderName())) {
+                contactDTO.setAddress3(cell.getStringCellValue());
+            }
         }
-        if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.COUNTRY.getHeaderName())) {
-            contactDTO.setCountry(cell.getStringCellValue());
+        if (mapColumnIndex.get(HeaderImportSubscribe.COUNTRY.getHeaderName()) != null) {
+            if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.COUNTRY.getHeaderName())) {
+                contactDTO.setCountry(cell.getStringCellValue());
+            }
         }
-        if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.CITY.getHeaderName())) {
-            contactDTO.setCity(cell.getStringCellValue());
+        if (mapColumnIndex.get(HeaderImportSubscribe.CITY.getHeaderName()) != null) {
+            if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.CITY.getHeaderName())) {
+                contactDTO.setCity(cell.getStringCellValue());
+            }
         }
-        if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.PHONE.getHeaderName())) {
-            contactDTO.setPhone(cell.getStringCellValue());
+        if (mapColumnIndex.get(HeaderImportSubscribe.PHONE.getHeaderName()) != null) {
+            if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.PHONE.getHeaderName())) {
+                contactDTO.setPhone(cell.getStringCellValue());
+            }
         }
-        if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.ZIP_CODE.getHeaderName())) {
-            contactDTO.setZipCode(cell.getStringCellValue());
+        if (mapColumnIndex.get(HeaderImportSubscribe.ZIP_CODE.getHeaderName()) != null) {
+            if (cell.getColumnIndex() == mapColumnIndex.get(HeaderImportSubscribe.ZIP_CODE.getHeaderName())) {
+                contactDTO.setZipCode(cell.getStringCellValue());
+            }
         }
-
 
         return contactDTO;
     }
