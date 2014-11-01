6a1f6fbc952bfdfae53726f867558fecb663ae48
Eldrid Aditya <zztop_aditya@yahoo.com>
2014-11-01 17:24:13 +0800

    Email verifier export done and remove import to contact button

--- src/test/java/com/equivi/mailsy/service/emailverifier/byteplant/BytePlantVerifierServiceImplIntegrationTest.java
+++ src/test/java/com/equivi/mailsy/service/emailverifier/byteplant/BytePlantVerifierServiceImplIntegrationTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:META-INF/spring-service.xml")
-public class BytePlantVerifierServiceImplTest {
+public class BytePlantVerifierServiceImplIntegrationTest {
 
     @Resource(name = "bytePlantVerifierService")
     private VerifierService emailVerifierService;
