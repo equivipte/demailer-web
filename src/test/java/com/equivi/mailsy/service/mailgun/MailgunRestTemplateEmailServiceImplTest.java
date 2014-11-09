b908838b2963bbb17d4b4e971551b944bb2add41
Eldrid Aditya <zztop_aditya@yahoo.com>
2014-11-10 02:11:14 +0800

    Fix unit test

--- src/test/java/com/equivi/mailsy/service/mailgun/MailgunRestTemplateEmailServiceImplTest.java
+++ src/test/java/com/equivi/mailsy/service/mailgun/MailgunRestTemplateEmailServiceImplTest.java
@@ -13,19 +13,11 @@ import org.mockito.MockitoAnnotations;
 import java.util.List;
 
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 public class MailgunRestTemplateEmailServiceImplTest {
-
-    @Mock
-    private DemailerRestTemplate demailerRestTemplate;
-
-    @Mock
-    private WebConfiguration webConfiguration;
-
-    @InjectMocks
-    private MailgunRestTemplateEmailServiceImpl mailgunEmailService;
 
     private static final String TO_LIST = "someone@email.com";
     private static final String CC_LIST = "cclist@email.com";
@@ -34,11 +26,15 @@ public class MailgunRestTemplateEmailServiceImplTest {
     private static final String EMAIL_CONTENT = "This is email content test";
     private static final String MAILGUN_URL = "http://mailgun.api.com";
     private static final String MAILGUN_API_KEY = "key-123";
-
     private static final String CAMPAIGN_ID = "8d2651d8-702b-4d5b-8233-94f6d647464b";
     private static final String DOMAIN = "equivitest.com";
     private static final String FROM = "POST Master ADMIN";
-
+    @Mock
+    private DemailerRestTemplate demailerRestTemplate;
+    @Mock
+    private WebConfiguration webConfiguration;
+    @InjectMocks
+    private MailgunRestTemplateEmailServiceImpl mailgunEmailService;
 
     @Before
     public void setUp() throws Exception {
@@ -64,7 +60,7 @@ public class MailgunRestTemplateEmailServiceImplTest {
         when(demailerRestTemplate.setHostName(anyString())).thenReturn(demailerRestTemplate);
         mailgunEmailService.sendMessage(CAMPAIGN_ID, DOMAIN, FROM, toList, ccList, bccList, TEST_EMAIL, EMAIL_CONTENT);
 
-        verify(demailerRestTemplate, times(3));
+        verify(demailerRestTemplate);
 
     }
 }
